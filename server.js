dbef520fe5c8d5d2bd5ca49a59c208ebcaf2c1f3
Beloise Huffstutler <me@beloisehuffstutler.codes>
2022-06-22 19:06:53 -0500

    Build mongoDB connection

--- server.js
+++ server.js
@@ -4,3 +4,14 @@ const cors = require('cors');
 const app = express();
 require('dotenv').config();
 
+let db,
+    dbConnectionString = process.env.DB_STRING
+    dbName = 'raccoonapi',
+    collection
+
+MongoClient.connect(dbConnectionString)
+    .then(client => {
+        console.log('Connected to Database')
+        db = client.db(dbName)
+        collection = db.collection('raccoonImages')
+    })
